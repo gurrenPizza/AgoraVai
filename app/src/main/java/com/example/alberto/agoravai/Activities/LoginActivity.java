9e45424b7ab03ff4dabf1f83bef7edf0c5d1be3d
Alberto Figuerêdo <34289440+gurrenPizza@users.noreply.github.com>
2018-10-05 14:48:03 -0300

    Atualizando Activity Login

--- app/src/main/java/com/example/alberto/agoravai/Activities/LoginActivity.java
+++ app/src/main/java/com/example/alberto/agoravai/Activities/LoginActivity.java
@@ -1,14 +1,78 @@
 package com.example.alberto.agoravai.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.alberto.agoravai.DAO.ConfiguracaoFirebase;
+import com.example.alberto.agoravai.Entidades.Usuarios;
 import com.example.alberto.agoravai.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private Button btnLogin;
+    private FirebaseAuth autenticacao;
+    private Usuarios usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = (EditText) findViewById(R.id.edtLoginEmail);
+        edtSenha = (EditText) findViewById(R.id.edtLoginSenha);
+        btnLogin = (Button) findViewById(R.id.btnLogar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!edtEmail.getText().toString().equals("") && edtSenha.getText().toString().equals("")){
+                    usuario = new Usuarios();
+
+                    usuario.setEmail(edtEmail.getText().toString());
+                    usuario.setSenha(edtSenha.getText().toString());
+
+                    validarLogin();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
+
+    private void validarLogin(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // ALL MIGHT DAYO
+                if(task.isSuccessful()){
+                    abrirTelaPrincipal();
+                    Toast.makeText(LoginActivity.this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void abrirTelaPrincipal(){
+        Intent abrirTelaPrincipal = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(abrirTelaPrincipal);
+    }
+
 }
